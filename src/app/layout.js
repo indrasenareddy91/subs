c4b5e39e25c4e4184a71b94294a8de34bbcdb850
IAMDBCOOPERR <reddyindra53@gmail.com>
2024-04-29 14:15:27 +0530

    adad;

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        style={{ margin: "0px", padding: "0px" }}
+        style={{ margin: "0px", padding: "0px", height: "100%" }}
         className={inter.className}
       >
         {children}
