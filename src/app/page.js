e6e8bba7f84c290ad4eaa4d5c74d495e022a508c
IAMDBCOOPERR <reddyindra53@gmail.com>
2024-04-29 14:05:21 +0530

    adad;

--- src/app/page.js
+++ src/app/page.js
@@ -97,12 +97,7 @@ const SearchBar = () => {
     backgroundImage: `linear-gradient(to right,transparent, transparent,rgb(20,24,28)), url(https://image.tmdb.org/t/p/original${randomMovie?.poster})`,
     backgroundSize: "cover",
     backgroundPosition: "center",
-    height: "100vh",
-    position: "fixed",
-    top: "0",
-    bottom: "0",
-    left: "0",
-    right: "0",
+    height: "100dvh",
   };
   return (
     <div style={style} className="backdrop">
