76f62764ce5ffd3bac7f23b01642a7eeed11d087
IAMDBCOOPERR <reddyindra53@gmail.com>
2024-09-29 16:38:53 +0530

    Inner peace

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import SearchBar from "../app/Searchbar";
 import { fetchRandomMovie } from "../actions/actions";
+export const dynamic = "force-dynamic";
 export default async function Home() {
   const initialRandomMovie = await fetchRandomMovie();
   console.log(initialRandomMovie);
