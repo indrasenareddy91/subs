8f01e38004aaef4bbcde0be33a0d0dbe85bab146
IAMDBCOOPERR <reddyindra53@gmail.com>
2024-10-07 07:59:54 +0530

    hello

--- src/app/subs/page.js
+++ src/app/subs/page.js
@@ -8,7 +8,7 @@ import "../index.css";
 import "./page.css";
 import SwapLoader from "./SwapLoader";
 import NotAvailable from "./NotAvailable";
-import Subswap from "./subswap";
+import Subswap from "./Subswap";
 import { searchMovies, findSubs } from "../../actions/actions";
 
 /******  32c72582-66fc-4b4b-9047-ffde2ed80740  *******/
