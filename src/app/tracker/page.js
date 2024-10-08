20f24736182f90423b0ec4936827eb05c9a713a7
IAMDBCOOPERR <reddyindra53@gmail.com>
2024-10-08 13:49:03 +0530

    traking...

--- src/app/tracker/page.js
+++ src/app/tracker/page.js
@@ -1,17 +1,10 @@
 import React from "react";
 import { sql } from "@vercel/postgres";
 
-const tracker = async () => {
-  const { rows } = await sql`SELECT * FROM movies`;
+const tracker = () => {
   return (
     <div>
       <h1>Movie Tracker</h1>
-      {rows.map((row) => (
-        <div key={row.id}>
-          <p>{row.movie_name}</p>
-          <p>{row.country}</p>
-        </div>
-      ))}
     </div>
   );
 };
