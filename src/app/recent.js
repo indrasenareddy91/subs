5086f0ce9d7c380d6f0e4e450e1ecc2673605716
IAMDBCOOPERR <reddyindra53@gmail.com>
2024-10-14 21:53:04 +0530

    oligbsd

--- src/app/recent.js
+++ src/app/recent.js
@@ -3,33 +3,20 @@ import "./recent.css";
 export default function RecentDownloads({ downloads }) {
   return (
     <div className="recent-downloads">
-      <div
-        className="card"
-        style={{
-          color: "white",
-          background: "black",
-          fontSize: "12px",
-        }}
-      >
+      <div className="card">
         <div className="card-header">
-          <span fontSize="14px">Recent Downloads</span>
+          <span>Recent Downloads</span>
         </div>
         <div className="card-content">
-          <div
-            className="downloads-list"
-            style={{
-              color: "white",
-              background: "black",
-            }}
-          >
+          <div className="downloads-list">
             {downloads.map((download) => {
               const [title, year] = download.movie_name.split(",");
               return (
                 <div key={download.id} className="download-item">
-                  <span>{title}</span>
                   <span>
-                    {year} - {download.country}
+                    {title}({year})
                   </span>
+                  <span> - {download.country}</span>
                 </div>
               );
             })}
