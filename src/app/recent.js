aad132acff652bcdd8b68c10b724987b2f18f860
IAMDBCOOPERR <reddyindra53@gmail.com>
2024-10-14 22:07:23 +0530

    oligbsd

--- src/app/recent.js
+++ src/app/recent.js
@@ -4,7 +4,7 @@ export default function RecentDownloads({ downloads }) {
   return (
     <div className="recent-downloads">
       <div className="card">
-        <div
+        <span
           className="card-header"
           style={{
             backgroundColor: "#f1c40f",
@@ -14,7 +14,7 @@ export default function RecentDownloads({ downloads }) {
           }}
         >
           Recent Downloads
-        </div>
+        </span>
         <div className="card-content">
           <div className="downloads-list">
             {downloads.map((download) => {
