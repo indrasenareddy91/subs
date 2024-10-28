a931fe744962916c07189e75d72ac716e34c8f1b
IAMDBCOOPERR <reddyindra53@gmail.com>
2024-10-28 12:22:57 +0530

    asfaf

--- src/app/api/track/route.js
+++ src/app/api/track/route.js
@@ -1,12 +1,57 @@
-// src/app/api/tracker/route.ts
 import { sql } from "@vercel/postgres";
 import axios from "axios";
 import { NextResponse } from "next/server";
 import { headers } from "next/headers";
-import { lookup } from "ip-location-api/pack";
-
+import { Console } from "console";
 export async function POST(request) {
-  return NextResponse.json({
-    message: "success",
-  });
+  try {
+    const body = await request.json();
+    const { moviename, year } = body;
+
+    if (!moviename) {
+      return NextResponse.json(
+        { error: "Movie named is required." },
+        { status: 400 }
+      );
+    }
+    const userAgent = request.headers.get("user-agent") || "";
+
+    // Get the IP address from headers
+    const headersList = headers();
+
+    const forwardedFor = headersList.get("x-forwarded-for");
+    const ip = forwardedFor ? forwardedFor.split(",")[0] : "127.0.0.1";
+    console.log("hekld");
+    // Get country from IP
+    const geoResponse = await axios.get(`https://ipapi.co/${ip}/json/`);
+    console.log(geoResponse);
+    const country = geoResponse.data.country_name || "Unknown";
+    const adress =
+      ip +
+      (geoResponse.data.city || "Unknown") +
+      ", " +
+      (geoResponse.data.org || "Unknown") +
+      ", " +
+      (geoResponse.data.region || "Unknown");
+
+    const movie = moviename + ", " + year;
+    // Insert into database
+
+    let data = await sql`
+            INSERT INTO movies (movie_name, country , adress , reference) 
+            VALUES (${movie}, ${country} , ${adress} , ${userAgent}) returning *
+        `;
+    return NextResponse.json(
+      { message: "Movie added successfully!", id: data.rows[0].movie_id },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error getting user location or inserting data:", error);
+    return NextResponse.json(
+      {
+        error: "An error occurred while saving the movie data.",
+      },
+      { status: 500 }
+    );
+  }
 }
