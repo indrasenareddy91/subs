c7834a650e45803aca46a1dc78d40edbf3789fe2
IAMDBCOOPERR <reddyindra53@gmail.com>
2024-10-28 20:53:51 +0530

    dsfsf

--- src/app/api/track/route.js
+++ src/app/api/track/route.js
@@ -1,55 +1,9 @@
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 import { headers } from "next/headers";
-import { lookup } from "ip-location-api/pack";
 
 export async function POST(request) {
-  try {
-    const body = await request.json();
-    const { moviename, year } = body;
-
-    if (!moviename) {
-      return NextResponse.json(
-        { error: "Movie named is required." },
-        { status: 400 }
-      );
-    }
-    const userAgent = request.headers.get("user-agent") || "";
-
-    // Get the IP address from headers
-    const headersList = headers();
-
-    const forwardedFor = headersList.get("x-forwarded-for");
-    const ip = forwardedFor ? forwardedFor.split(",")[0] : "127.0.0.1";
-    console.log("hekld");
-    // Get country from IP
-    const response = await fetch(`https://ipapi.co/${ip}/json`);
-    const geoResponse = await response.json();
-    const adress =
-      ip +
-      ", " +
-      (geoResponse.country_name || "Unknown") +
-      ", " +
-      (geoResponse.city || "Unknown");
-
-    const movie = moviename + ", " + year;
-    // Insert into database
-
-    let data = await sql`
-            INSERT INTO movies (movie_name, country , adress , reference) 
-            VALUES (${movie}, ${country} , ${adress} , ${userAgent}) returning *
-        `;
-    return NextResponse.json(
-      { message: "Movie added successfully!", id: data.rows[0].movie_id },
-      { status: 201 }
-    );
-  } catch (error) {
-    console.error("Error getting user location or inserting data:", error);
-    return NextResponse.json(
-      {
-        error: "An error occurred while saving the movie data.",
-      },
-      { status: 500 }
-    );
-  }
+  return NextResponse.json({
+    message: "shsh",
+  });
 }
