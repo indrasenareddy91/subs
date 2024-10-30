be54bb05be97c7515e75fe72fcfcd2ccc2f1ffb0
IAMDBCOOPERR <reddyindra53@gmail.com>
2024-10-30 12:21:53 +0530

    what the fuckkkkkk

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 import SearchBar from "../app/Searchbar";
 import { fetchRandomMovie, trendingtoday } from "../actions/actions";
+import { connection } from "next/server";
 
 import { sql } from "@vercel/postgres";
 import random from "random";
 export const revalidate = 43200; // 12 hours
-export const dynamic = "force-dynamic";
 export default async function Home() {
   const initialRandomMovie = await fetchRandomMovie();
   const trendingmovies = await trendingtoday();
+  console.log("this runs only once ervy 12 hours");
+  await connection();
+  console.log("this run every requwst");
   const trending = random.choice([...trendingmovies]);
   const randomMovie = random.choice([...initialRandomMovie.data]);
   const { rows: recentdownloads } = await sql`
