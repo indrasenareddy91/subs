f67c01b051f89bb980574c8105e68a64d3a36005
IAMDBCOOPERR <reddyindra53@gmail.com>
2024-11-01 07:59:58 +0530

    afafdz

--- src/app/Searchbar.js
+++ src/app/Searchbar.js
@@ -236,7 +236,6 @@ const SearchBar = ({ randomMovie, recentdownloads, trending }) => {
             <Link
               className={`sublinks ${index}`}
               ref={(el) => (linkRefs.current[index] = el)}
-              prefetch={true}
               style={{
                 width: "580px",
                 padding: "10px",
