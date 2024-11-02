ea1176081eb6d022943bf6e3f4401cd759ef1d87
IAMDBCOOPERR <reddyindra53@gmail.com>
2024-11-02 14:21:10 +0530

    done

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -60,7 +60,12 @@ const findSubs = async (movieId, lang) => {
 };
 export async function trendingtoday() {
   const trending = await fetch(
-    "https://trakt-trending-movies.reddyindra53.workers.dev/api/trending"
+    "https://trakt-trending-movies.reddyindra53.workers.dev/api/trending",
+    {
+      next: {
+        revalidate: 10 * 60 * 60,
+      },
+    }
   );
   const tr = await trending.json();
   console.log("isnide fuck thiss ", tr);
