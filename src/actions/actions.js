a260ab174b97945e6a1308b736b36565537d0601
IAMDBCOOPERR <reddyindra53@gmail.com>
2024-11-04 10:23:42 +0530

    dfd

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -12,7 +12,7 @@ export async function fetchRandomMovie() {
   try {
     const response = await fetch(url, {
       next: {
-        revalidate: 120,
+        revalidate: 1200,
       },
     });
     if (!response.ok) {
@@ -66,7 +66,7 @@ export async function trendingtoday() {
     "https://trakt-trending-movies.reddyindra53.workers.dev/api/trending",
     {
       next: {
-        revalidate: 120,
+        revalidate: 1200,
       },
     }
   );
