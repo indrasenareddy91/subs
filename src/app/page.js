ffda249dc77ec1d8c235020b58c1870a89f1db95
Indrasena reddy <88037874+indrasenareddy91@users.noreply.github.com>
2024-12-01 17:46:51 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -11,24 +11,12 @@ export default async function Home() {
   const trendingmovies = await trendingtoday();
   const trending = random.choice([...trendingmovies]);
   const randomMovie = random.choice([...initialRandomMovie]);
-  const { rows: recentdownloads } = await sql`
-  SELECT 
-     movie_name, 
-     MIN(country) AS country,  -- Choose the first country for each (movie_name, ip) pair
-     MIN(movie_id) AS movie_id,  -- Choose the first movie_id for each (movie_name, ip) pair
-     ip
- FROM movies
- WHERE ip IS NOT NULL
- GROUP BY movie_name, ip
- ORDER BY movie_id DESC
- LIMIT 4;
- `;
+
   return (
     <main style={{ height: "100%" }}>
       <SearchBar
         trending={trending}
         randomMovie={randomMovie}
-        recentdownloads={recentdownloads}
       />
     </main>
   );
